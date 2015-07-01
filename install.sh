82cdd820e92d12f8b86dbb6ffdb9ca50df22d9b5
Jeremy Pry <JPry@users.noreply.github.com>
2015-07-01 16:22:46 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install FBI tool
+sudo apt-get install fbi
+
+# Update fstab
+# todo: Conditional check first
+sudo echo "/dev/sda1	/home/pi/digital_picture_frame/media	vfat	uid=pi,gid=pi,umask=0022,async,auto,nosuid,ro,nouser,nofai	0	2" >> /etc/fstab
